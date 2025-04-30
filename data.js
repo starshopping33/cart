1fa3b1b8e08a98b9267815f3dfa75684f9f05f4c
kauan santos <kauanprsantos123@gmail.com>
2025-04-30 17:10:13 -0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,25 @@
+export const produtos = [
+    { 
+      id: 1, 
+      nome: "iPhone 7 128GB",
+      price: 2500,
+      description: 'iPhone 7 com 128GB de armazenamento',
+      images: [
+        'https://m.media-amazon.com/images/I/51rxO1J7xML._AC_SX679_.jpg',
+        'https://m.media-amazon.com/images/I/71VR7LOD5BL._AC_SX679_.jpg',
+        'https://m.media-amazon.com/images/I/61-ipUY2CWL._AC_SX679_.jpg',
+        'https://m.media-amazon.com/images/I/61bXvV8aJbL._AC_SX679_.jpg'
+      ]
+    },
+    {
+      id: 2,
+      nome: "Smart TV Samsung Série 4",
+      price: 1399.90,
+      description: 'Smart TV Samsung 32" LED Plana',
+      images: [
+        'https://m.media-amazon.com/images/I/91UsHjAPTlL._AC_SX450_.jpg',
+        'https://m.media-amazon.com/images/I/81RQfUxv8VL._AC_SX450_.jpg',
+        'https://m.media-amazon.com/images/I/71LJJrKbezL._AC_SX450_.jpg'
+      ]
+    }
+]
